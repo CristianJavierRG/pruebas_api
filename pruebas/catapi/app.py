f41be28cdf8ee3812ed629767de3055ce6dcd359
Chupapi0 <cristianjaviergayosso@gmail.com>
2026-01-23 14:45:17 +0000

    GATOS api

---
+++ pruebas/catapi/app.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+api_key = "live_7NJGZaisRFcQewUy1qnYS779chPlFf5F4XTDLhKKZZ5m2dGeV5VDpr6HsiyOL1RO"
+url = "https://api.thecatapi.com/v1/images/search"
+limit = "limit=10"
+
+"""
+https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t
+"""
+# https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8tj
